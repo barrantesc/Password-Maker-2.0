743028bfa8e13aee0981255a6ae60aa3f4bc1218
playground <connie.s.barrantes@gmail.com>
2021-11-21 18:59:05 -0500

    added loop, ocations for if choosing outside parameters

--- Develop/script.js
+++ Develop/script.js
@@ -2,31 +2,34 @@
 var generateBtn = document.querySelector("#generate");
 
 // Set variables for password generator and test with console.log
-var upper = ['A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'];
-var lower = ['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'];
-var number = ['0,1,2,3,4,5,6,7,8,9'];
-var symbol = ['~,`,!,@,#,$,%,^,&,*,(,),=,+,{,},<,>,?'];
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var symbol = "~`!@#$%^&*()=+{}<>?";
 
 console.log(upper, lower, number, symbol);
 // Do I need to set declare the password as well? Ex: var password; password = upper, lower, number, symbol;?
 // Do I need to assign a var for length? 
 
 //User input variables
-var confirmLength = "";
+var confirmPasswordLength = "";
 var confirmUpper;
 var confirmLower;
 var confirmNumber;
 var confirmSymbol;
 
 
-
 function generatePassword() {
+  var combineCharacters = ""
   console.log("Hey! You clicked the button!");
 
   // 1. Prompt the user for the password criteria
   //    a. Password Length 8 to 128
   var confirmLength = window.prompt("How long would you like your password to be? (Choose between 8 to 128 characters)");
-
+if (confirmLength < 8 || confirmLength > 128) {
+    alert("Please chose a character between 8 and 128");
+    return ""
+  }
   //    b. Lowercase, uppercase, numbers, special characters
   //       - Press okay to confirm and cancel to decline
   confirmUpper = confirm("Do you want uppercase letters?(Select OK to confirm and CANCEL to decline)");
@@ -34,14 +37,40 @@ function generatePassword() {
   confirmNumber = confirm("Do you want numbers?(Select OK to confirm and CANCEL to decline)");
   confirmSymbol = confirm("Do you want symbols?(Select OK to confirm and CANCEL to decline)");
   
+  if (confirmUpper) {
+    combineCharacters += upper;
+  }
+
+  if (confirmLower) {
+    combineCharacters += lower;
+  }
+
+  if (confirmNumber) {
+    combineCharacters += number;
+  }
+
+  if (confirmSymbol) {
+    combineCharacters += symbol;
+  }
+
+  if (combineCharacters.length === 0) {
+    alert("Please choose at least one type of character")
+    return ""
+  }
+  
+  var randomPassword = "";
   // 2. Validate the input
   //    a. Loop for if another number is chosen outside range
-  
-  // 3. Generate Password
+  for(var i = 0; i < confirmLength; i++) {
 
+    // 3. Generate Password   
+    var randomIndex = Math.floor(Math.random() * combineCharacters.length);
+    randomPassword += combineCharacters[randomIndex]
+
+  }
 
   // 4. Display password to the page
-return "Generated password will go here!";
+return randomPassword;
 }
 
 // Write password to the #password input
